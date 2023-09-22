6fa2e5353f70f50ab4c2539be8a83ac5247c00e8
Neena MS <98210299+neena-ms@users.noreply.github.com>
2023-09-22 23:26:42 +0530

    Create exprtree.h

---
+++ stage1/exercise3/exprtree.h
@@ -0,0 +1,16 @@
+typedef struct tnode
+{
+    int val;
+    char *op;
+    struct tnode *left,*right;
+}tnode;
+
+struct tnode* makeLeafNode(int n);
+struct tnode* makeOperatorNode(char c,struct tnode *l,struct tnode *r);
+int evaluate (struct tnode *t);
+void prefix(struct tnode *t);
+void postfix(struct tnode *t);
+int getReg();
+int freeReg();
+void assembly(struct tnode *t,FILE *fptr);
+void result(FILE *fptr);
