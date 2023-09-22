8a80fa8535120a27bf815d10b14ff85d86ac2700
Neena MS <98210299+neena-ms@users.noreply.github.com>
2023-09-22 23:30:01 +0530

    Create exprtree.h

---
+++ stage2/exercise1/exprtree.h
@@ -0,0 +1,13 @@
+typedef struct tnode
+{
+    int val;
+    int type;
+    char* varname;
+    int nodetype; // leaf=0,read=1,write=2,connector=3,operator=4
+    struct tnode *left,*right;
+}tnode;
+
+struct tnode* makeLeafNodenum(int n);
+struct tnode* createTree(int val,int type, char *c,struct tnode *l,struct tnode *r);
+struct tnode* makeLeafNodevar(char c);
+int evaluate(struct tnode *t);
